af5f1055c2b96790c2a63174733488401d059273
Jack Ziesing <jack.ziesing@jziesing-ltm5.internal.salesforce.com>
2021-07-16 00:45:45 -0700

    newnewsshhijijddsds

--- worker.js
+++ worker.js
@@ -46,10 +46,10 @@ function start() {
     // if (Math.random() < 0.05) {
     //   throw new Error("This job failed!")
     // }
-    const titles = ['Continent', 'Country', 'City'];
-    const continents = ['North America', 'South America', 'Australia', 'Asia', 'Africa', 'Antartica', 'Europe'];
+    var titles = ['Continent', 'Country', 'City'];
+    var continents = ['North America', 'South America', 'Australia', 'Asia', 'Africa', 'Antartica', 'Europe'];
     var newThings =  [];
-    for(let i=0; i<1500; i++) {
+    for(var i=0; i<1500; i++) {
 
         let newTitle = titles[getRandomInt(3)];
         console.log('newTitle :: ' + newTitle);
