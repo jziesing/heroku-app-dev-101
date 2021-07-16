fda4d7f69b331e1a1e001c835c24ec18b9b29cab
Jack Ziesing <jack.ziesing@jziesing-ltm5.internal.salesforce.com>
2021-07-15 23:25:04 -0700

    worker attempt 3

--- worker.js
+++ worker.js
@@ -54,17 +54,17 @@ function start() {
         switch (newTitle) {
             case 'City':
                 for(let i=0; i<1500; i++)  {
-                    newThings.push([title: 'City', description: randomGen.city()]);
+                    newThings.push(['title': 'City', 'description': randomGen.city()]);
                 }
                 break;
             case 'Country':
                 for(let i=0; i<1500; i++)  {
-                    newThings.push([title: 'Country', description: randomCountry({ full: true })]);
+                    newThings.push(['title': 'Country', 'description': randomCountry({ full: true })]);
                 }
                 break;
             case 'Continent':
                 for(let i=0; i<1500; i++)  {
-                    newThings.push([title: 'Continent', description: continents[getRandomInt(7)]]);
+                    newThings.push(['title': 'Continent', 'description': continents[getRandomInt(7)]]);
                 }
                 break;
         }
