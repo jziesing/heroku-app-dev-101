a491896687148e8f864d31e6d2ad254d76f58f26
Jack Ziesing <jack.ziesing@jziesing-ltm5.internal.salesforce.com>
2021-07-19 16:12:00 -0700

    loggin  debuggg

--- worker.js
+++ worker.js
@@ -32,20 +32,7 @@ workQueue.process((job, done) => {
 
 	  let newTitle = titles[0];
 	  console.log('newTitle :: ' + newTitle);
-	  switch (newTitle) {
-		  // case 'City':
-		  //     newThings.push(['City',  randomGen.city() ]);
-		  //     break;
-		  // case 'Country':
-		  //     newThings.push(['Country',  randomCountry({ full: true }) ]);
-		  //     break;
-		  case 'Continent':
-			  newThings.push([{'Continent':  continents[getRandomInt(6)]} ]);
-			  break;
-		  default:
-			 break;
-	  }
-
+      newThings.push({'Continent':  continents[getRandomInt(6)]} );
   }
 
   console.log('madeee data');
